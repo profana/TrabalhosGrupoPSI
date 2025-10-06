1f71a4b5359e12d8ea84a928a98bab833316307a
Ana Esteves <11687547+profana@users.noreply.github.com>
2025-10-06 22:10:54 +0100

    1 commit após asneira

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numeros = new int[10];
+
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                Console.Write("Introduza o número:  ");
+                numeros[i] = int.Parse(Console.ReadLine());
+            }
+
+            int maximo = numeros[0];
+            int indice = 0;
+
+            for (int i = 1; i < numeros.Length; i++)
+            {
+                if (numeros[i] > maximo)
+                {
+                    maximo = numeros[i];
+                    indice = i;
+                }
+            }
+            Console.WriteLine("O maior valor é {0} e encontra-se no índice {1}.", maximo, indice);
+            Console.WriteLine("Maior número: {0}, índice: {1}.", maximo, indice);
+            Console.ReadKey();
+        }
+    }
+}
